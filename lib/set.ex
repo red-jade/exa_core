85c5f4928fb1cb10af8c73d23e854c520d07384a
red-jade <katagorikal@gmail.com>
2024-09-09 16:13:25 +0700

    Handle arithmetic errors and empty set for min/max/sum

--- lib/set.ex
+++ lib/set.ex
@@ -13,26 +13,40 @@ defmodule Exa.Set do
   Get the minimum value in a set,
   or error if the set is empty.
   """
-  @spec min(MapSet.t()) :: any() | :error
+  @spec min(MapSet.t()) :: any() | {:error, any()}
   def min(ms) when is_set(ms) do
     if MapSet.size(ms) == 0, do: :error, else: Enum.reduce(ms, &Kernel.min/2)
+  rescue
+    err -> {:error, err}
   end
 
   @doc """
-  Get the maximum value in a set,
-  or error if the set is empty.
+  Get the maximum value in a set.
+  Return error if the set is empty or contains non-numerica data.
   """
-  @spec max(MapSet.t()) :: any() | :error
+  @spec max(MapSet.t()) :: any() | {:error, any()}
   def max(ms) when is_set(ms) do
-    if MapSet.size(ms) == 0, do: :error, else: Enum.reduce(ms, &Kernel.max/2)
+    if MapSet.size(ms) == 0 do
+      {:error, "Empty set"}
+    else
+      Enum.reduce(ms, &Kernel.max/2)
+    end
+  rescue
+    err -> {:error, err}
   end
 
   @doc """
-  Sum values in a set,
-  or error if the set is empty.
+  Sum values in a set.
+  Return error if the set is empty or contains non-numerica data.
   """
   @spec sum(MapSet.t()) :: any() | :error
   def sum(ms) when is_set(ms) do
-    if MapSet.size(ms) == 0, do: :error, else: Enum.reduce(ms, &Kernel.+/2)
+    if MapSet.size(ms) == 0 do
+      {:error, "Empty set"}
+    else
+      Enum.reduce(ms, &Kernel.+/2)
+    end
+  rescue
+    err -> {:error, err}
   end
 end
