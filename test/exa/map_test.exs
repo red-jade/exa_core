0fde04739a6ce1401f022ddf9e7094048e45b9a9
red-jade <katagorikal@gmail.com>
2024-10-03 15:23:17 +0700

    Add inverse look-up function keys/2

--- test/exa/map_test.exs
+++ test/exa/map_test.exs
@@ -32,10 +32,14 @@ defmodule Exa.MapTest do
     assert "c" == key(bij, 3)
     assert "a" == key(bij, 1)
     assert is_nil(key(bij, 99))
+    assert ["b"] == keys(bij, 2)
+    assert [] == keys(bij, 99)
 
     multi = %{:a => 1, :b => 1, :c => 3}
     assert key(multi, 3) == :c
     assert key(multi, 1) in [:a, :b]
     assert is_nil(key(multi, 99))
+    assert keys(multi, 3) == [:c]
+    assert Enum.sort(keys(multi, 1)) == [:a, :b]
   end
 end
