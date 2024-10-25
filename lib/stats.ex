d3994c5442ec6b06e42d163305b86d9c8ce8dd8f
red-jade <katagorikal@gmail.com>
2024-10-25 13:48:41 +0700

    Simplify - remove functions that return count

--- lib/stats.ex
+++ lib/stats.ex
@@ -2,60 +2,78 @@ defmodule Exa.Stats do
   @moduledoc "Statistical utilities."
 
   import Exa.Types
+  alias Exa.Types, as: E
 
   @doc """
-  Count and mean of a non-empty list of numbers.
+  Mean of a non-empty list of numbers.
 
   ## Examples
-      iex> n_mean([1,2,3])
-      {3, 2.0}
+      iex> mean([1,2,3])
+      2.0
   """
-  @spec n_mean([number(), ...]) :: {n :: E.count1(), mean :: float()}
-  def n_mean(xs) when is_list_nonempty(xs) do
-    {n, s} = len_sum(xs, 0, 0)
-    {n, s / n}
+  @spec mean([number()]) :: float()
+  def mean(xs) when is_list_nonempty(xs), do: Enum.sum(xs) / length(xs)
+
+  @doc """
+  Mean and variance of a non-empty list of numbers.
+
+  ## Examples
+      iex> mean_var([1,2,3])
+      {2.0, 2/3}
+  """
+  @spec mean_var([number(), ...]) :: {mean :: float(), variance :: float()}
+  def mean_var(xs) when is_list_nonempty(xs) do
+    {n, u} = n_mean(xs)
+    {u, sumsqd(xs, u) / n}
   end
 
   @doc """
-  Count, mean and variance of a non-empty list of numbers.
+  Variance of a non-empty list of numbers, given the mean.
 
   ## Examples
-      iex> n_mean_var([1,2,3])
-      {3, 2.0, 2/3}
+      iex> var([1,2,3], 2.0)
+      2/3
   """
-  @spec n_mean_var([number(), ...]) :: {n :: E.count1(), mean :: float(), variance :: float()}
-  def n_mean_var(xs) when is_list_nonempty(xs) do
-    {n, s} = len_sum(xs, 0, 0)
-    u = s / n
-    {n, u, do_sumsqd(xs, u, 0) / n}
+  @spec var([number(), ...], float()) :: float()
+  def var(xs, u) when is_list_nonempty(xs) and is_float(u) do
+    do_var(xs, u)
   end
 
   @doc """
-  Count, mean and standard deviation of a non-empty list of numbers.
+  Mean and standard deviation of a non-empty list of numbers.
 
   ## Examples
-      iex> n_mean_var([1,2,3])
-      {3, 2.0, 2/3}
+      iex> mean_sd([1,2,3])
+      {2.0, Exa.Math.sqrt(2/3)}
   """
-  @spec n_mean_sd([number(), ...]) :: {n :: E.count1(), mean :: float(), sd :: float()}
-  def n_mean_sd(xs) when is_list_nonempty(xs) do
-    {n, s} = len_sum(xs, 0, 0)
-    u = s / n
-    {n, u, :math.sqrt(do_sumsqd(xs, u, 0) / n)}
+  @spec mean_sd([number(), ...]) :: {mean :: float(), sd :: float()}
+  def mean_sd(xs) when is_list_nonempty(xs) do
+    {n, u} = n_mean(xs)
+    {u, Exa.Math.sqrt(sumsqd(xs, u) / n)}
   end
 
   @doc """
-  Count and Root Mean Square (RMS) of a non-empty list of numbers.
+  Standard deviation of a non-empty list of numbers,
+  given the mean.
 
   ## Examples
-      iex> n_rms([1,2,3])
-      {3, 2.160246899469287}
+      iex> sd([4,5,6], 5.0)
+      Exa.Math.sqrt(2/3)
   """
-  @spec n_rms([number(), ...]) :: {n :: E.count1(), rms :: float()}
-  def n_rms(xs) when is_list_nonempty(xs) do
-    {n, sq} = len_sumsq(xs, 0, 0)
-    {n, :math.sqrt(sq / n)}
+  @spec sd([number(), ...], float()) :: float()
+  def sd(xs, u) when is_list_nonempty(xs) and is_float(u) do
+    Exa.Math.sqrt(var(xs, u))
   end
+
+  @doc """
+  Root Mean Square (RMS) of a non-empty list of numbers.
+
+  ## Examples
+      iex> rms([3,4,5])
+      5 * Exa.Math.sqrt(2/3)
+  """
+  @spec rms([number(), ...]) :: float()
+  def rms(xs) when is_list_nonempty(xs), do: do_rms(xs)
 
   @doc """
   Get the mode(s) of a non-empty list.
@@ -71,7 +89,7 @@ defmodule Exa.Stats do
       iex> mode([1])
       {1, [1]}
   """
-  @spec mode([any()]) :: {count :: E.count1(), modes :: [any()]}
+  @spec mode([t, ...]) :: {count :: E.count1(), modes :: [t]} when t: var
 
   def mode([_] = is), do: {1, is}
 
@@ -79,30 +97,82 @@ defmodule Exa.Stats do
     is |> Enum.frequencies() |> Enum.reduce({0, []}, &max_freq/2)
   end
 
+  @doc """
+  Get the median(s) of a non-empty list.
+
+  The list is sorted, then the median depends on the length of the list:
+  - odd: central value in the sorted list (element at `div(n,2)`)
+  - even: the two central values (elements `n/2, n/2 + 1`)
+
+  In the case of an even number of number values,
+  the client may choose to average the two central values. 
+
+  For even number of values, if the two central values are equal,
+  then only one is returned.
+
+  ## Examples:
+      iex> median([1,2,3,4,5])
+      3
+      iex> median([6,5,4,3,2,1])
+      {3,4}
+      iex> median([4,2,2,1])
+      2
+      iex> median([:foo, :bar, :baz])
+      :baz
+      iex> median([10,9])
+      {9,10}
+  """
+  @spec median([t, ...]) :: t | {t, t} when t: var
+  def median([x]), do: x
+  def median([x, y]) when x < y, do: {x, y}
+  def median([x, x]), do: x
+  def median([x, y]), do: {y, x}
+  def median(xs) when is_list_nonempty(xs), do: do_med(length(xs), Enum.sort(xs))
+
   # -----------------
   # private functions
   # -----------------
 
   # sum of square of differences to mean
-  @spec do_sumsqd([number()], float(), float()) :: number()
-  defp do_sumsqd([x | xs], u, sq), do: do_sumsqd(xs, u, sq + sq(x - u))
-  defp do_sumsqd([], _u, sq), do: sq
+  @spec sumsqd([number(), ...], float(), float()) :: float()
+  defp sumsqd(xs, u, sq \\ 0.0)
+  defp sumsqd([x | xs], u, sq), do: sumsqd(xs, u, sq + sq(x - u))
+  defp sumsqd([], _u, sq), do: sq
 
-  # length and sum of squares in a single pass
-  @spec len_sumsq([number()], non_neg_integer(), float()) :: {non_neg_integer(), number()}
-  defp len_sumsq([x | xs], n, sq), do: len_sumsq(xs, n + 1, sq + sq(x))
-  defp len_sumsq([], n, sq), do: {n, sq}
+  # variance - average sum of square of differences to mean
+  @spec do_var([number(), ...], float(), {non_neg_integer(), float()}) :: float()
+  defp do_var(xs, u, n_sq \\ {0, 0.0})
+  defp do_var([x | xs], u, {n, sq}), do: do_var(xs, u, {n + 1, sq + sq(x - u)})
+  defp do_var([], _u, {n, sq}), do: sq / n
 
-  # length and sum in a single pass
-  @spec len_sum([number()], non_neg_integer(), number()) :: {non_neg_integer(), number()}
-  defp len_sum([x | xs], n, s), do: len_sum(xs, n + 1, s + x)
-  defp len_sum([], n, s), do: {n, s}
+  # root mean square in a single pass
+  @spec do_rms([number(), ...], non_neg_integer(), float()) :: float()
+  defp do_rms(xs, n \\ 0, sq \\ 0.0)
+  defp do_rms([x | xs], n, sq), do: do_rms(xs, n + 1, sq + sq(x))
+  defp do_rms([], n, sq), do: Exa.Math.sqrt(sq / n)
+
+  # count and mean in a single pass
+  @spec n_mean([number(), ...], non_neg_integer(), float()) :: {non_neg_integer(), float()}
+  defp n_mean(xs, n \\ 0, s \\ 0.0)
+  defp n_mean([x | xs], n, s), do: n_mean(xs, n + 1, s + x)
+  defp n_mean([], n, s), do: {n, s / n}
 
   # reducer to find max count values from frequencies
   @spec max_freq({any(), E.count1()}, {E.count(), [any()]}) :: {E.count1(), [any()]}
   defp max_freq({i, n}, {nmax, _is}) when n > nmax, do: {n, [i]}
   defp max_freq({i, nmax}, {nmax, is}), do: {nmax, [i | is]}
   defp max_freq(_, acc), do: acc
+
+  # get median from count and sorted list
+  @spec do_med(E.count1(), [t]) :: t | {t, t} when t: var
+  defp do_med(n, s) when is_int_odd(n), do: Enum.at(s, div(n, 2))
+
+  defp do_med(n, s) do
+    case Enum.drop(s, div(n, 2) - 1) do
+      [x, x | _] -> x
+      [x, y | _] -> {x, y}
+    end
+  end
 
   # square a number
   @spec sq(number()) :: number()
